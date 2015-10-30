f287abcda4d0e2506cc61f71621cde6cb19f3ca3
Elva Chao <elvac8866@gmail.com>
2015-10-30 10:30:34 -0700

    print something

---
+++ printsomething/ReadMe.md
@@ -0,0 +1,4 @@
+printf() is a standard function. Each compiler will support this function. Between the () is stated what has to be printed (on screen). Create a C-program that prints the following when executed:
+My first C-program
+is a fact!
+Good, isn’t it?
---
+++ printsomething/homeworkex131.cpp
@@ -0,0 +1,8 @@
+#include<iostream>  // a preprocessor directive, include the contents of the iostream file to access the istream and ostream functions and objects
+int main()   // main() function header
+{    // beginning of the function body of main () function
+	printf("My first C-program\nis a fact!\nGood, isn't it? \n");  // print this message onto the screen prompt, where \n is indicates the start 
+	//of a new line and the words following the \n will appear on the next line
+	system("PAUSE");   // use this to pause the screen prompt from disppearing after all the source codes are being compiled and executed
+	return 0;  //return the integer 0 to the main() function to end the main() function
+}
